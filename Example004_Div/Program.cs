99880c62d66fb309709afb2fd61eb3cf72656ddc
Sergei Dydin <dydin_serg@mail.ru>
2023-03-24 12:43:48 +0300

    fix in Example004_Div

--- Example004_Div/Program.cs
+++ Example004_Div/Program.cs
@@ -1,4 +1,6 @@
-﻿double numberA = 12;
-double numberB = 5;
-double result = numberA / numberB;
-Console.WriteLine(result);
+﻿double numberA=10;
+double numberB=3;
+Console.WriteLine(numberA/numberB);
+
+
+ 
